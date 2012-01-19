e6462e6f38991ea0ea85c75d8e12c0dadbe36d1a
Ross Foley <ross3141@gmail.com>
2012-01-18 19:40:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mi6stats::Application.config.secret_token = '0f9d486640011cbfaba738c48c2b2af6f31eb30247a40381a81cb3adb1caeddce748201d16108ca6173d5cd73cb5519f7b7f9e16ccb4e86878c36235026a07d2'
