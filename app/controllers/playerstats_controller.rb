f6c7099f1cf166d8d59e121ff790ab7e3fc7f687
Ross Foley <ross3141@gmail.com>
2012-01-19 20:28:28 -0500

    Adjusted order and styling of iframe table

--- app/controllers/playerstats_controller.rb
+++ app/controllers/playerstats_controller.rb
@@ -2,7 +2,7 @@ class PlayerstatsController < ApplicationController
 
   include ActionView::Helpers::NumberHelper
 
-  QUERY = "SELECT `tbl_playerstats`.*, `tbl_playerdata`.SoldierName FROM `tbl_playerstats` INNER JOIN tbl_server_player ON tbl_playerstats.StatsID = tbl_server_player.StatsID JOIN tbl_playerdata ON tbl_server_player.PlayerID = tbl_playerdata.PlayerID"
+  QUERY = "SELECT `tbl_playerstats`.*, `tbl_playerdata`.SoldierName FROM `tbl_playerstats` INNER JOIN tbl_server_player ON tbl_playerstats.StatsID = tbl_server_player.StatsID JOIN tbl_playerdata ON tbl_server_player.PlayerID = tbl_playerdata.PlayerID ORDER BY Score DESC"
 
   def index
     @players = Playerstats.find_by_sql(QUERY)
