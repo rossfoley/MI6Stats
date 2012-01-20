966509e27fe0113b316e729c8ea96166474f25d6
Ross Foley <ross3141@gmail.com>
2012-01-19 23:29:21 -0500

    Refactored SQL to be much more readable

--- app/controllers/playerstats_controller.rb
+++ app/controllers/playerstats_controller.rb
@@ -1,31 +1,17 @@
 class PlayerstatsController < ApplicationController
-
-  include ActionView::Helpers::NumberHelper
-
-  QUERY = "SELECT `tbl_playerstats`.*, `tbl_playerdata`.SoldierName FROM `tbl_playerstats` INNER JOIN tbl_server_player ON tbl_playerstats.StatsID = tbl_server_player.StatsID JOIN tbl_playerdata ON tbl_server_player.PlayerID = tbl_playerdata.PlayerID ORDER BY Score DESC"
-
   def index
-    @players = Playerstats.find_by_sql(QUERY)
-
-    headshot_data = Playerstats.count(:group => "FLOOR(Headshots/10)")
-    @pie_data = "[#{generate_pie_data(headshot_data)}]"
+    @players = Playerstats.all_with_names.order("Playtime DESC")
+    @pie_data = "[#{generate_pie_data}]"
   end
 
   def table
-    @players = Playerstats.find_by_sql(QUERY)
+    @players = Playerstats.all_with_names.order("Score DESC")
   end
 
   private
 
-  def generate_pie_data(data)
+  def generate_pie_data
+    data = Playerstats.count(:group => "FLOOR(Headshots/10)")
     data.map { |k, v| "['#{k*10}-#{k*10+9}', #{v}]" }.join(", ")
-  end
-
-  def generate_categories(data)
-    data.map { |k, v| "'#{k*10}-#{k*10+9}'" }.join(", ")
-  end
-
-  def generate_values(data, total_players)
-    data.map { |k, v| "#{number_with_precision((v*100.0)/total_players, :precision => 2)}" }.join(", ")
   end
 end
--- app/models/playerstats.rb
+++ app/models/playerstats.rb
@@ -1,3 +1,4 @@
 class Playerstats < ActiveRecord::Base
   set_table_name "tbl_playerstats"
+  scope :all_with_names, select("`tbl_playerstats`.*, `tbl_playerdata`.SoldierName").joins("INNER JOIN tbl_server_player ON tbl_playerstats.StatsID = tbl_server_player.StatsID INNER JOIN tbl_playerdata ON tbl_server_player.PlayerID = tbl_playerdata.PlayerID")
 end
