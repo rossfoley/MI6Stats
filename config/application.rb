d13721c3609add452eb1ff1d85639d45f38e7ffc
Ross Foley <ross3141@gmail.com>
2012-01-20 23:19:36 -0500

    Precompile assets

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Mi6stats
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['playerstats.js', 'mapstats.js']
   end
 end
